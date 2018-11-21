1e324e42c53abbe0523d219ca4024261b5923b26
Austin Passy <367897+thefrosty@users.noreply.github.com>
2018-11-21 10:37:13 -0800

    Change navigation to hook into sidebar action tag. Remove return when only 1 section is registered.

--- src/views/setting/nav.php
+++ src/views/setting/nav.php
@@ -1,24 +1,28 @@
 <?php declare(strict_types=1);
 
 use Dwnload\WpSettingsApi\Api\SettingSection;
+use Dwnload\WpSettingsApi\App;
 use Dwnload\WpSettingsApi\Settings\SectionManager;
 
-/** @var $this \Dwnload\WpSettingsApi\WpSettingsApi */
+/** @var $this Dwnload\WpSettingsApi\WpSettingsApi */
 $sections = SectionManager::getSections($this->getApp()->getMenuSlug());
 
-if (count($sections) <= 1) {
-    return;
-}
-?>
-<ul class="Dwnload_WP_Settings_Api__menu">
-    <?php
-    /** @var SettingSection $section */
-    foreach ($sections as $section) {
+add_action(App::ACTION_PREFIX . 'settings_sidebars', function () {
+    echo '<ul class="Dwnload_WP_Settings_Api__menu">';
+}, 0);
+
+/** @var SettingSection $section */
+foreach ($sections as $key => $section) {
+    ++$key;
+    add_action(App::ACTION_PREFIX . 'settings_sidebars', function () use ($section) {
         printf(
             '<li><a href="javascript:;" data-tab-id="Dwnload_WP_Settings_Api__%s">%s</a></li>',
             esc_attr($section->getId()),
             esc_html($section->getTitle())
         );
-    }
-    ?>
-</ul>
+    }, ++$key * 2 + 1);
+}
+
+add_action(App::ACTION_PREFIX . 'settings_sidebars', function () {
+    echo '</ul>';
+}, 199);
