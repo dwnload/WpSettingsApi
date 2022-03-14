421c92908d0b38fe7b7726c8c5e5e06e564e1d6d
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-03-14 13:30:21 -0700

    Add new Admin Settings hook constants.

--- src/ActionHookName.php
+++ src/ActionHookName.php
@@ -9,6 +9,9 @@ namespace Dwnload\WpSettingsApi;
 interface ActionHookName
 {
 
+    public const ADMIN_SETTINGS_ADMIN_SCRIPTS = WpSettingsApi::FILTER_PREFIX . 'admin_scripts';
+    public const ADMIN_SETTINGS_ADMIN_STYLES = WpSettingsApi::FILTER_PREFIX . 'admin_styles';
+    public const ADMIN_SETTINGS_LOCALIZE_SCRIPT = WpSettingsApi::FILTER_PREFIX . 'localize_script';
     public const AFTER_SETTINGS_SECTIONS_FORM = WpSettingsApi::ACTION_PREFIX . 'after_settings_sections_form';
     public const FORM_BOTTOM = WpSettingsApi::ACTION_PREFIX . 'form_bottom';
     public const FORM_TOP = WpSettingsApi::ACTION_PREFIX . 'form_top';
