b82a886b636b9b3aa3d9ab44eade68d69458d76b
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-05-19 09:30:55 -0700

    Add new date fields (#70)
    * Add new date and datetime fields.
    
    * PHP8+ code cleanup.
    
    * Code cleanup
    
    * Remove

--- src/Api/PluginSettings.php
+++ src/Api/PluginSettings.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Dwnload\WpSettingsApi\Api;
 
@@ -13,146 +15,101 @@ class PluginSettings extends BaseModel
 {
 
     /** @var string $nonce_s */
-    protected $nonce_s = WpSettingsApi::FILTER_PREFIX . '%s';
+    protected string $nonce_s = WpSettingsApi::FILTER_PREFIX . '%s';
 
     /** @var string $domain */
-    private $domain;
+    private string $domain;
 
     /** @var string $file */
-    private $file;
+    private string $file;
 
     /** @var string $menu_slug */
-    private $menu_slug;
+    private string $menu_slug;
 
     /** @var string $menu_title */
-    private $menu_title;
+    private string $menu_title;
 
     /** @var string $page_title */
-    private $page_title;
+    private string $page_title;
 
     /** @var string $prefix */
-    private $prefix;
+    private string $prefix;
 
     /** @var string $version */
-    private $version;
+    private string $version;
 
-    /**
-     * @param string $domain
-     */
-    public function setDomain(string $domain)
-    {
-        $this->domain = $domain;
-    }
-
-    /**
-     * @return string
-     */
     public function getDomain(): string
     {
         return $this->domain;
     }
 
-    /**
-     * @param string $file
-     */
-    public function setFile(string $file)
+    protected function setDomain(string $domain)
     {
-        $this->file = $file;
+        $this->domain = $domain;
     }
 
-    /**
-     * @return string
-     */
     public function getFile(): string
     {
         return $this->file;
     }
 
-    /**
-     * @return string
-     */
+    protected function setFile(string $file)
+    {
+        $this->file = $file;
+    }
+
     public function getNonce(): string
     {
         return \sprintf($this->nonce_s, \plugin_basename($this->getFile()));
     }
 
-    /**
-     * @param string $slug
-     */
-    public function setMenuSlug(string $slug)
-    {
-        $this->menu_slug = $slug;
-    }
-
-    /**
-     * @return string
-     */
     public function getMenuSlug(): string
     {
         return $this->menu_slug ?? $this->domain;
     }
 
-    /**
-     * @param string $title
-     */
-    public function setMenuTitle(string $title)
+    protected function setMenuSlug(string $slug)
     {
-        $this->menu_title = $title;
+        $this->menu_slug = $slug;
     }
 
-    /**
-     * @return string
-     */
     public function getMenuTitle(): string
     {
         return $this->menu_title;
     }
 
-    /**
-     * @param string $title
-     */
-    public function setPageTitle(string $title)
+    protected function setMenuTitle(string $title)
     {
-        $this->page_title = $title;
+        $this->menu_title = $title;
     }
 
-    /**
-     * @return string
-     */
     public function getPageTitle(): string
     {
         return $this->page_title;
     }
 
-    /**
-     * @param string $prefix
-     */
-    public function setPrefix(string $prefix)
+    protected function setPageTitle(string $title)
     {
-        $this->prefix = $prefix;
+        $this->page_title = $title;
     }
 
-    /**
-     * @return string
-     */
     public function getPrefix(): string
     {
         return $this->prefix;
     }
 
-    /**
-     * @param string $version
-     */
-    public function setVersion(string $version)
+    protected function setPrefix(string $prefix)
     {
-        $this->version = $version;
+        $this->prefix = $prefix;
     }
 
-    /**
-     * @return string
-     */
     public function getVersion(): string
     {
         return $this->version;
+    }
+
+    protected function setVersion(string $version)
+    {
+        $this->version = $version;
     }
 }
