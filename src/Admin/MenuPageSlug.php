5a3afb990c7bcdda4f1d81ec95ca4afd09d16c19
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-15 13:12:07 -0800

    Class constant type is missing

--- src/Admin/MenuPageSlug.php
+++ src/Admin/MenuPageSlug.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Dwnload\WpSettingsApi\Admin;
 
@@ -7,9 +9,7 @@ namespace Dwnload\WpSettingsApi\Admin;
  *
  * This class defines constants to use as the parent slug for attaching submenu
  * pages to built-in WordPress admin pages.
- *
  * @since 2.2.0
- *
  * @package Dwnload\WpSettingsApi\Admi
  */
 interface MenuPageSlug
@@ -18,16 +18,16 @@ interface MenuPageSlug
     /**
      * Slugs to use as parents for the built-in WordPress menu pages.
      */
-    public const DASHBOARD = 'index.php';
-    public const POSTS = 'edit.php';
-    public const MEDIA = 'upload.php';
-    public const PAGES = self::POSTS . '?post_type=page';
-    public const COMMENTS = 'edit-comments.php';
-    public const CUSTOM_POST_TYPE_S = self::POSTS . '?post_type=%s';
-    public const APPEARANCE = 'themes.php';
-    public const PLUGINS = 'plugins.php';
-    public const USERS = 'users.php';
-    public const TOOLS = 'tools.php';
-    public const SETTINGS = 'options-general.php';
-    public const NETWORK_SETTINGS = 'settings.php';
+    public const string DASHBOARD = 'index.php';
+    public const string POSTS = 'edit.php';
+    public const string MEDIA = 'upload.php';
+    public const string PAGES = self::POSTS . '?post_type=page';
+    public const string COMMENTS = 'edit-comments.php';
+    public const string CUSTOM_POST_TYPE_S = self::POSTS . '?post_type=%s';
+    public const string APPEARANCE = 'themes.php';
+    public const string PLUGINS = 'plugins.php';
+    public const string USERS = 'users.php';
+    public const string TOOLS = 'tools.php';
+    public const string SETTINGS = 'options-general.php';
+    public const string NETWORK_SETTINGS = 'settings.php';
 }
